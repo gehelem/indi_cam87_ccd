eec451e3112cb42c36a6d50da41260a054304982
gehelem <gilles@joag.fr>
2021-04-12 16:42:03 +0200

    Same player, shoot again - work in progress

---
+++ indi_cam87_ccd.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="CCDs">
+        <device label="Cam87 CCD" mdpd="true">
+                <driver name="Cam87 CCD">indi_cam87_ccd</driver>
+                <version>@CAM87_VERSION_MAJOR@.@CAM87_VERSION_MINOR@</version>
+	</device>
+</devGroup>
+</driversList>
